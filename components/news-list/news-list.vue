01936af0e2ac0432a6bd353b36d51c54317ffe13
NDxy <714335240@qq.com>
2022-03-22 23:18:48 +0800

    dev

---
+++ components/news-list/news-list.vue
@@ -0,0 +1,122 @@
+<template>
+	<view class="news-list">
+		<view class="header_icon">
+			<image class="icon" :src="header" mode=""></image>
+		</view>
+		<view class="title_massge">
+			<view class="title">{{title}}</view>
+			<view class="massge">{{massge}}</view>
+		</view>
+		<view class="time_status">
+			<view class="time">{{time}}</view>
+			<view class="status">{{statusCheck}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'NewsList',
+		props: {
+			title: {
+				type: String,
+				default: ''
+			}, // 标题
+			massge: {
+				type: String,
+				default: ''
+			}, // 扩展信息
+			header: {
+				type: String,
+				default: ''
+			}, // 头像
+			status: {
+				type: String,
+				default: ''
+			}, // 缩略图
+			time: {
+				type: String,
+				default: ''
+			} //日期
+		},
+		methods: {
+			onClick() {
+				this.$emit('click')
+			}
+		},
+		computed: {
+			statusCheck(){
+				let statusTxt = "1"
+				switch (this.status){
+					case 1:
+						statusTxt = "在线"
+						break;
+					case 2:
+						statusTxt = "免扰"
+						break;
+					case 3:
+						statusTxt = "禁言"
+						break;
+					default:
+						break;
+				}
+				return statusTxt
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.news-list {
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: row;
+		padding: 24upx;
+		position: relative;
+		
+		.header_icon {
+			width: 110upx;
+			height: 110upx;
+			.icon{
+				width: 110upx;
+				height: 110upx;
+			}
+		}
+		
+		.title_massge,.time_status {
+			display: flex;
+			flex-direction: column;
+			padding: 12upx 24upx;
+			justify-content: space-between;
+			.title{
+				color: #383838;
+				font-weight: 700;
+				font-size: 32upx;
+			}
+			.massge,.time,.status{
+				color: #9a9a9a;
+				font-size: 24upx;
+			}
+			
+		}
+		
+		.title_massge{
+			box-sizing: border-box;
+			width: calc(100% - 244upx);
+		}
+		
+		.time_status{
+			width: 110upx;
+		}
+		
+		&::after{
+			content: "";
+			width: calc(100% - 134upx);
+			position: absolute;
+			bottom: 0;
+			right: 0;
+			height: 1px;
+			background-color: #c9c9c9;
+		}
+	}
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,8 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="news_lists">
+			<news-list title="莫莫" time="21:30" massge="嗯啊 爱你哟 小宝贝" header="../../static/logo.png"></news-list>
 		</view>
 	</view>
 </template>
@@ -30,23 +29,7 @@
 		align-items: center;
 		justify-content: center;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.news_lists{
+		width: 100%;
 	}
 </style>
