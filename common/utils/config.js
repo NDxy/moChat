763529936ded52294d00bc91a5e627c96bd4d513
NDxy <714335240@qq.com>
2022-05-06 07:43:07 +0800

    dev api

--- common/utils/config.js
+++ common/utils/config.js
@@ -6,28 +6,13 @@ const WX_APPID = 'wx4015a847131516ad'
 const ALI_APPID = '2021001157673614'  
 
 if(process.env.NODE_ENV === 'development'){
-    // requestURL = 'http://192.168.0.25:15001'
-	// imgURL = 'http://192.168.0.25:15001'
-    // requestURL = 'http://open-test.mynatapp.cc'
-	// imgURL = 'http://open-test.mynatapp.cc'
-	// milkRequest = 'https://open-test.mynatapp.cc'
-	// milkRequest = 'https://open-test.mynatapp.cc'
- //    requestURL = 'https://xiaoda.mynatapp.cc'
-	// imgURL = 'https://xiaoda.mynatapp.cc'
-	milkRequest = 'https://milk.sndcard.com'
-	requestURL = 'https://sndcard.com'
-	imgURL = 'https://sndcard.com'
-	appid = 'wxf9bf13bc7fbf0cbc'
+	milkRequest = '81.68.225.182:38003'
+	requestURL = '81.68.225.182:38003'
+	imgURL = '81.68.225.182:38003'
 }else{
     requestURL = 'https://xiaoda.mynatapp.cc'
 	imgURL = 'https://xiaoda.mynatapp.cc'
 	milkRequest = 'https://open-test.mynatapp.cc'
-	// milkRequest = 'https://milk.sndcard.com'
-    // requestURL = 'http://open-test.mynatapp.cc'
-	// imgURL = 'http://open-test.mynatapp.cc'
-	// requestURL = 'https://sndcard.com'
-	// imgURL = 'https://sndcard.com'
-	appid = 'wxd0572409648a8520'
 }
 
 export { appid, imgURL, requestURL, WX_APPID, milkRequest }
